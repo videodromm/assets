3f56623efac7c63b610d94d1a8d747e2cb4478b7
Bruce LANE <bruce@batchass.fr>
2016-12-01 11:44:28 +0100

    shaders

---
+++ AudioUnknown.glsl
@@ -0,0 +1,26 @@
+void main(void)
+{
+   vec2 uv = iZoom * gl_TexCoord[0].st;
+   uv.x -= iRenderXY.x;
+   uv.y -= iRenderXY.y;
+   uv = 2. * uv;
+   uv = uv - 1.;
+	float b = texture2D(iAudio0, vec2(0.1, 0.0)).x;
+	b = b*b;
+	float c = texture2D(iAudio0, vec2(0.2, 0.0)).x;
+	c = c*c;
+	float d = texture2D(iAudio0, vec2(0.3, 0.0)).x;
+	d = d*d;
+	float e = texture2D(iAudio0, vec2(0.05, 0.0)).x * 5.0;
+	
+	float a = length(vec2(.5,.5)-uv);
+	float angle = atan(uv.y,uv.x)*(6.0+e);
+	a = a*(cos(angle+b+c+d+iGlobalTime)*0.5+1.0);
+		
+	vec4 col = texture2D(iAudio0, vec2(a, 0.0));
+	col.x += b;
+	col.y += c;
+	col.z += d;
+  gl_FragColor = vec4(col.x, col.y, col.z,1.0);
+}
+
---
+++ DeskFirstShader.glsl
@@ -0,0 +1,23 @@
+// https://www.shadertoy.com/view/Xs2GWz
+float pix(vec2 p)
+{
+    p=abs(p);
+    vec2 q;
+    q.x=float(int(p.x+0.5));
+    q.y=float(int(p.y+0.5));
+    vec2 r=p-q;
+    if(max(abs(r.x),abs(r.y))>0.45) return iColor.b;
+	//float v = sin(length(q)*0.5-iGlobalTime*(iRotationSpeed+1.0)*5.0);//5.0);
+	float v = sin(length(q)*0.5-iGlobalTime*5.0);//5.0);
+	if(v>0.0) return iColor.r;
+	else return iColor.g;
+}
+
+void main(void)
+{
+	float width = iZoom / min( iResolution.x, iResolution.y );
+	vec2 control = mix( iResolution.xy * 0.5, iMouse.xy, 1.0 - step( iMouse.z, 0.0 ) );
+	vec2 uv = ( gl_FragCoord.xy - control ) * width * iRatio;//25.0;
+	
+	gl_FragColor = vec4(pix(uv)*iColor.r,pix(uv)*iColor.g,pix(uv)*iColor.b,iAlpha);
+}
